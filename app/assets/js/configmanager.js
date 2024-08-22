59d39a13588b8cf774e7755d73ec90bc8dd099fd
Heriptik <theomiot@gmail.com>
2024-08-22 17:23:52 +0200

    Modification nom : PixelmonUltra-1.20.2

--- app/assets/js/configmanager.js
+++ app/assets/js/configmanager.js
@@ -7,7 +7,7 @@ const logger = LoggerUtil.getLogger('ConfigManager')
 
 const sysRoot = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME)
 
-const dataPath = path.join(sysRoot, '.Pixelmon-Ultra-1.20.1')
+const dataPath = path.join(sysRoot, '.PixelmonUltra-1.20.2')
 
 const launcherDir = require('@electron/remote').app.getPath('userData')
 
